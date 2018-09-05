4668bcbd3cb07fe42fda0c0d42dfa5d6234d877c
Radosław Kwiatkowski <35296705+radkwiat84@users.noreply.github.com>
2018-09-05 10:35:17 +0200

    add CmmandLineRunner
    added commandLIneRunner to check saving into db

--- src/main/java/radkwiat84/RodzinaBackendApplication.java
+++ src/main/java/radkwiat84/RodzinaBackendApplication.java
@@ -1,12 +1,27 @@
 package radkwiat84;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class RodzinaBackendApplication {
+import radkwiat84.model.Family;
+import radkwiat84.repositories.FamilyRepository;
 
+@SpringBootApplication
+public class RodzinaBackendApplication implements CommandLineRunner{
+
+	@Autowired
+	private FamilyRepository familyRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RodzinaBackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		familyRepository.save(new Family());
+		familyRepository.save(new Family());
+		
 	}
 }
